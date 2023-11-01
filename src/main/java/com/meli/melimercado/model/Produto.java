19f86978afcc5fecd993d82f216f5ea9f7bde1da
joaocruzmeli <joao.vcruz@mercadolivre.com>
2023-11-01 17:52:49 -0300

    build: configurado banco mysql

--- src/main/java/com/meli/melimercado/model/Produto.java
+++ src/main/java/com/meli/melimercado/model/Produto.java
@@ -8,7 +8,11 @@ public class Produto {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column
     private String nome;
+
+    @Column
     private Double valor;
 
     public Produto() {
